c7fc9afbd8ee71d2dfbd761658e2a5314d0fecf4
Speedy-Lex <78314533+speedy-lex@users.noreply.github.com>
2025-03-14 23:46:07 +0100

    fix double collision in 1 frame causing crash
    yes the whole thing had to be reworked

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use raylib::prelude::*;
 
 fn main() {
     let (mut rl, rt) = init()
+        .vsync()
         .size(1200, 1000)
         .build();
 
@@ -17,43 +18,42 @@ fn main() {
         size: 400.0,
         mass: 100.0,
     };
-    let mut collisions = 0;
-    rl.set_target_fps(10000);
+    let mut collisions: usize = 0;
+    let mut is_next_block_block = true;
 
     while !rl.window_should_close() {
         let dt = rl.get_frame_time() as f64;
-        let block1_np = dt*block1.vel + block1.pos;
-        let block2_np = dt*block2.vel + block2.pos;
-        let block1_range = (block1_np-block1.size/2.0)..(block1_np+block1.size/2.0);
-        let block2_range = (block2_np-block2.size/2.0)..(block2_np+block2.size/2.0);
+        let mut rem_t = dt;
+        while rem_t > 0.0 {
+            let collision_t = if is_next_block_block {
+                get_block_block_collision_t(&block1, &block2)
+            } else {
+                get_wall_block_collision_t(&block1)
+            };
 
-        if block1_range.contains(&block2_range.start) || block1_range.contains(&block2_range.end) {
-            collisions += 1;
+            let collision = collision_t <= rem_t && collision_t >= 0.0;
 
-            let total_vel = block1.vel - block2.vel;
-            let dist = (block2.pos - block2.size/2.0) - (block1.pos + block1.size/2.0);
-            let t = dist / total_vel;
+            if collision {
+                rem_t -= collision_t;
+                collisions += 1;
 
-            assert!(t > 0.0);
-            assert!(t <= dt);
+                block1.pos += block1.vel * collision_t;
+                block2.pos += block2.vel * collision_t;
 
-            block1.pos += block1.vel * t;
-            block2.pos += block2.vel * t;
-
-            let rem_t = dt - t;
-
-            let combined_mass = block1.mass + block2.mass;
-            let block1_new_v = ((block1.mass - block2.mass) * block1.vel + 2.0 * block2.mass * block2.vel) / combined_mass;
-            let block2_new_v = ((block2.mass - block1.mass) * block2.vel + 2.0 * block1.mass * block1.vel) / combined_mass;
-            
-            block1.vel = block1_new_v;
-            block2.vel = block2_new_v;
-
-            update_wall_block(&mut block1, rem_t, &mut collisions);
-            block2.pos += block2.vel * rem_t;            
-        } else {
-            update_wall_block(&mut block1, dt, &mut collisions);
-            block2.pos = block2_np;
+                if is_next_block_block {
+                    collide_blocks(&mut block1, &mut block2);
+                } else {
+                    collide_wall_block(&mut block1);
+                }
+                is_next_block_block = !is_next_block_block;
+            } else {
+                block1.pos += block1.vel * rem_t;
+                block2.pos += block2.vel * rem_t;
+                break;
+            }
+            if collisions % 1000000 == 0 {
+                println!("{rem_t}: {collisions}");
+            }
         }
 
         let mut d = rl.begin_drawing(&rt);
@@ -62,32 +62,32 @@ fn main() {
         block1.draw(&mut d, 1000);
         block2.draw(&mut d, 1000);
 
-        d.draw_rectangle(0, 0, 100, 1000, Color::WHITE);
+        d.draw_rectangle(0, 0, 100, 1000, Color::DARKBLUE);
         d.draw_fps(10, 10);
         d.draw_text(&format!("{collisions}"), 10, 30, 20, Color::GREEN);
     }
 }
 
+fn collide_wall_block(block: &mut Block) {
+    block.vel = -block.vel; // fully reflect velocity
+}
+fn get_wall_block_collision_t(block: &Block) -> f64 {
+    (block.pos - block.size/2.0) / -block.vel // distance of leading edge to wall / velocity
+}
 
-fn update_wall_block(block: &mut Block, t: f64, collisions: &mut usize) {
-    let new_pos = block.pos + block.vel * t;
-    if new_pos >= block.size/2.0 {
-        block.pos = new_pos;
-        return;
-    }
-    *collisions += 1;
-
-    let coll_t = (block.pos - block.size/2.0) / -block.vel;
-
-    assert!(coll_t > 0.0);
-    assert!(coll_t <= t);
-
-    let rem_t = t - coll_t;
-
-    block.vel = -block.vel;
-    // FIXME: we need recursive collision checking like collide and slide :/ (do the collisions on line 30-53 here as well)
-    // ignoring rem_t is fine here since the velocities are correct but we *should* use it
-    // block.pos = block.size / 2.0 + block.vel * rem_t;
+fn collide_blocks(block1: &mut Block, block2: &mut Block) {
+    // derived from conservation of energy and momentum
+    let combined_mass = block1.mass + block2.mass;
+    let block1_new_v = ((block1.mass - block2.mass) * block1.vel + 2.0 * block2.mass * block2.vel) / combined_mass;
+    let block2_new_v = ((block2.mass - block1.mass) * block2.vel + 2.0 * block1.mass * block1.vel) / combined_mass;
+    
+    block1.vel = block1_new_v;
+    block2.vel = block2_new_v;
+}
+fn get_block_block_collision_t(block1: &Block, block2: &Block) -> f64 {
+    let total_vel = block1.vel - block2.vel;
+    let dist = (block2.pos - block2.size/2.0) - (block1.pos + block1.size/2.0);
+    dist / total_vel
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -99,6 +99,6 @@ struct Block {
 }
 impl Block {
     fn draw(&self, d: &mut RaylibDrawHandle, h: i32) {
-        d.draw_rectangle((self.pos+100.0-self.size/2.0) as i32, h - self.size as i32, self.size as i32, self.size as i32, Color::WHEAT);
+        d.draw_rectangle((self.pos+100.0-self.size/2.0) as i32, h - self.size as i32, self.size as i32, self.size as i32, Color::BLUE);
     }
 }
